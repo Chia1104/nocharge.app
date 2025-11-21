8a6c1af8c133e3dfa139594d2f634006627dffc2
Chia1104 <yuyuchia7423@gmail.com>
2025-11-21 16:29:20 +0800

    fix(app): fix locale init issue

--- apps/app/src/translations/index.ts
+++ apps/app/src/translations/index.ts
@@ -2,16 +2,18 @@ import { getLocales } from "expo-localization";
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
+import { kv } from "@/libs/storage/kv";
+
 import * as resources from "./resources";
-import { getLng } from "./utils";
+import { codeToLocale } from "./utils";
 
 const ns = Object.keys(Object.values(resources)[0]);
 export const defaultNS = ns[0];
 
 const init = () => {
-  let savedLng = getLng();
+  let savedLng = kv.getString("lng");
   if (!savedLng) {
-    savedLng = getLocales()[0].languageCode ?? "en";
+    savedLng = codeToLocale(getLocales()[0].languageCode ?? "en");
   }
   i18n
     .use(initReactI18next)
@@ -19,7 +21,7 @@ const init = () => {
       ns,
       defaultNS,
       resources: Object.fromEntries(Object.entries(resources)),
-      lng: "en",
+      lng: savedLng,
       fallbackLng: "en",
       interpolation: {
         escapeValue: false, // not needed for react as it escapes by default
